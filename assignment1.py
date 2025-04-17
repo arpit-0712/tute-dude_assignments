0b61cae4554935c814ab631ff0cf70b18efd8628
arpit-0712 <senger.0712@gmail.com>
2025-04-17 21:41:16 +0530

    Remove External file

--- assignment1.py
+++
@@ -1,7 +0,0 @@
-num1 = int(input("Enter the first num"))
-num2 = int(input("Enter 2nd value"))
-
-print("Addition: ",num1+num2)
-print("Subtraction: ",num1-num2)
-print("Multiplication: ",num1*num2)
-print("Division: ",num1/num2)
