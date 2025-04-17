d9cfd4791434dba70dec37f28347ab1b8a601ca7
arpit-0712 <senger.0712@gmail.com>
2025-04-17 21:17:32 +0530

    assignment 1 and 2

---
+++ assignment/assignment2.py
@@ -0,0 +1,9 @@
+
+# Getting the first and last name from user
+first_name = input("Enter the user first name: ")
+last_name = input("Enter the user last name: ")
+
+
+full_name = first_name +" "+last_name + "!"
+
+print("Hello,",full_name,"Welcome to the Python program.")
